911507d68fe46c283c3dfa855f753ee9d6ffca96
Divyang Patel <divyang.jp@gmail.com>
2021-03-09 01:49:04 +1100

    [WIP] things coming together

--- AWS-Terraform/main.tf
+++ AWS-Terraform/main.tf
@@ -2,6 +2,41 @@ data "aws_subnet_ids" "private_subs" {
   vpc_id = module.network.vpc-id
 
   tags = {
-    Tier = "Private"
+    type = "Private"
   }
+}
+
+data "aws_ami" "ubuntu-1604" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
+module "network" {
+  
+}
+
+module "app-instances" {
+  source = "./modules/ec2"
+  for_each = data.aws_subnet_ids.private_sub.ids
+  ami = "${data.aws_ami.ubuntu-1604.id}"
+  instance_type = "t3.micro"
+  subnet_name = each.value
+  key-name = "${module.key-pair.key-name}"
+  name_tag = "AppInst0${index(data.aws_subnet_ids.private_subs.ids, each.value) + 1}"
+}
+
+module "key-pair" {
+  source = "./modules/key-pair"
+  key_name = "ctest-key-pair"
 }
